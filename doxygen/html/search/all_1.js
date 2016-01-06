6bb5282b834e3cdb61b46cc20a69eec6878116e8
Isaac Mushinsky <itz@mushinsky.net>
2016-01-05 23:03:48 -0500

    remove generated files

--- doxygen/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['set_5fhc_5fdate',['set_hc_date',['../d4/dc7/hconverter_8h.html#afa4b54c2a64591d108e91187259561e0',1,'hconverter.c']]]
-];
--- doxygen/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hconverter_2eh',['hconverter.h',['../d4/dc7/hconverter_8h.html',1,'']]]
-];
