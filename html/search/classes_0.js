a801887e505d826c3fb905d25ff367601d1691d7
Isaac Mushinsky <itz@mushinsky.net>
2016-01-06 00:12:20 -0500

    doc change

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['hc_5fcal_5fimpl_5fs',['hc_cal_impl_s',['../d4/d98/structhc__cal__impl__s.html',1,'']]],
-  ['hc_5fdate_5fs',['hc_date_s',['../d0/d36/structhc__date__s.html',1,'']]],
-  ['heb_5ftime_5fs',['heb_time_s',['../db/d68/structheb__time__s.html',1,'']]]
+  ['hc_5fcal_5fimpl_5fs',['hc_cal_impl_s',['../structhc__cal__impl__s.html',1,'']]],
+  ['hc_5fdate_5fs',['hc_date_s',['../structhc__date__s.html',1,'']]],
+  ['heb_5ftime_5fs',['heb_time_s',['../structheb__time__s.html',1,'']]]
 ];
--- html/search/files_0.js
+++ html/search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hconverter_2eh',['hconverter.h',['../d4/dc7/hconverter_8h.html',1,'']]]
+  ['hconverter_2eh',['hconverter.h',['../hconverter_8h.html',1,'']]]
 ];
