8344f3bff3b03e0acc936371554fb46b0b484e1f
imush <imush@users.noreply.github.com>
2026-09-14 03:34:40 +0000

    deploy: 98d6a70c5f9353cdec3f538d6b8c1f6f2e3a7fd1

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -3,5 +3,9 @@ var searchData=
   ['hc_5fchametz_5fburn_0',['HC_CHAMETZ_BURN',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aa98542e7b233e70595511772fff9126d6',1,'hc_jewish_dates.h']]],
   ['hc_5fchametz_5fdispose_1',['HC_CHAMETZ_DISPOSE',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aa25db1829a37ae79253e793386da8e48c',1,'hc_jewish_dates.h']]],
   ['hc_5fchametz_5feat_2',['HC_CHAMETZ_EAT',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aace946c328d2c704e7e1f86ca8c9fd88d',1,'hc_jewish_dates.h']]],
-  ['hc_5fhaft_5focc_5fweekly_3',['HC_HAFT_OCC_WEEKLY',['../hc__haftarah_8h.html#a1852c412aa7177b989205ae8341ac03caddaa692fd05cf3a7407d09251f4fe70f',1,'hc_haftarah.h']]]
+  ['hc_5ffast_5fbegins_5fcandles_3',['HC_FAST_BEGINS_CANDLES',['../hc__jewish__dates_8h.html#ae400a4b274f905c95f015bdd82dbe3e1a4a8b62145cbf47baee1265145c8fc2f7',1,'hc_jewish_dates.h']]],
+  ['hc_5ffast_5fbegins_5fdawn_4',['HC_FAST_BEGINS_DAWN',['../hc__jewish__dates_8h.html#ae400a4b274f905c95f015bdd82dbe3e1ade378c302bb9c3c07c593277860adf10',1,'hc_jewish_dates.h']]],
+  ['hc_5ffast_5fbegins_5fsunset_5',['HC_FAST_BEGINS_SUNSET',['../hc__jewish__dates_8h.html#ae400a4b274f905c95f015bdd82dbe3e1a65486a3bc4f3dc9ea8639beb733b7acb',1,'hc_jewish_dates.h']]],
+  ['hc_5ffast_5fends_5fnightfall_6',['HC_FAST_ENDS_NIGHTFALL',['../hc__jewish__dates_8h.html#ae400a4b274f905c95f015bdd82dbe3e1a7426a92d082dd598f56bba3ca4738bfe',1,'hc_jewish_dates.h']]],
+  ['hc_5fhaft_5focc_5fweekly_7',['HC_HAFT_OCC_WEEKLY',['../hc__haftarah_8h.html#a1852c412aa7177b989205ae8341ac03caddaa692fd05cf3a7407d09251f4fe70f',1,'hc_haftarah.h']]]
 ];
