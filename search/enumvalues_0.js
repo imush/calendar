4e5abce5613c84f47f29803e5166999398064fce
imush <imush@users.noreply.github.com>
2026-09-14 02:34:56 +0000

    deploy: 7ad161a8d68ff0febe8d3be71967d0a356c63116

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['hc_5fhaft_5focc_5fweekly_0',['HC_HAFT_OCC_WEEKLY',['../hc__haftarah_8h.html#a1852c412aa7177b989205ae8341ac03caddaa692fd05cf3a7407d09251f4fe70f',1,'hc_haftarah.h']]]
+  ['hc_5fchametz_5fburn_0',['HC_CHAMETZ_BURN',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aa98542e7b233e70595511772fff9126d6',1,'hc_jewish_dates.h']]],
+  ['hc_5fchametz_5fdispose_1',['HC_CHAMETZ_DISPOSE',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aa25db1829a37ae79253e793386da8e48c',1,'hc_jewish_dates.h']]],
+  ['hc_5fchametz_5feat_2',['HC_CHAMETZ_EAT',['../hc__jewish__dates_8h.html#ae2ed1b159fb3c7ad25ecd3b0c170406aace946c328d2c704e7e1f86ca8c9fd88d',1,'hc_jewish_dates.h']]],
+  ['hc_5fhaft_5focc_5fweekly_3',['HC_HAFT_OCC_WEEKLY',['../hc__haftarah_8h.html#a1852c412aa7177b989205ae8341ac03caddaa692fd05cf3a7407d09251f4fe70f',1,'hc_haftarah.h']]]
 ];
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,15 +1,16 @@
 var searchData=
 [
   ['hc_5fcalendar_5ftype_0',['hc_calendar_type',['../hconverter_8h.html#ab2e9a61a6892e93f715495524b637181',1,'hconverter.h']]],
-  ['hc_5fdate_1',['hc_date',['../hconverter_8h.html#ad05914392daaa102a0ef2fbf6346164d',1,'hconverter.h']]],
-  ['hc_5fday_5fof_5fweek_2',['hc_day_of_week',['../hconverter_8h.html#ad459dce317e77962658ecd25c98b10e2',1,'hconverter.h']]],
-  ['hc_5fhaftarah_5foccasion_3',['hc_haftarah_occasion',['../hc__haftarah_8h.html#afe10dd8a90b2b2d186d76cea1fa3e3c6',1,'hc_haftarah.h']]],
-  ['hc_5fparsha_4',['hc_parsha',['../parshiot_8h.html#a9d1c07ea899fce1bf359f83d526a130c',1,'parshiot.h']]],
-  ['hc_5freading_5fslot_5',['hc_reading_slot',['../hc__torah__reading_8h.html#adc847556ffef0d26a6405ef66fae4967',1,'hc_torah_reading.h']]],
-  ['hc_5fseason_6',['hc_season',['../tekufot_8h.html#a1f1d5a4e9d7f8d54f297f0d8d4ddd057',1,'tekufot.h']]],
-  ['hc_5fspecial_5fday_7',['hc_special_day',['../hc__jewish__dates_8h.html#a37d72f3c286a8f63fe58b1ecc8ab9168',1,'hc_jewish_dates.h']]],
-  ['hc_5ftekufa_5fopinion_8',['hc_tekufa_opinion',['../tekufot_8h.html#ab0524668f0bb43054d90a43222eb146f',1,'tekufot.h']]],
-  ['hc_5fzmanim_9',['hc_zmanim',['../zmanim_8h.html#a1568769eb6a59bd7fb3182cb9764e4f9',1,'zmanim.h']]],
-  ['heb_5ftime_10',['heb_time',['../hconverter_8h.html#ac7a4cd5d0d3b38beae2c34b7ce198938',1,'hconverter.h']]],
-  ['heb_5fyear_5ftype_11',['heb_year_type',['../hconverter_8h.html#add5736c8efbe5e180304777603254226',1,'hconverter.h']]]
+  ['hc_5fchametz_1',['hc_chametz',['../hc__jewish__dates_8h.html#ae6f61896d9c29feae3cf875009602f02',1,'hc_jewish_dates.h']]],
+  ['hc_5fdate_2',['hc_date',['../hconverter_8h.html#ad05914392daaa102a0ef2fbf6346164d',1,'hconverter.h']]],
+  ['hc_5fday_5fof_5fweek_3',['hc_day_of_week',['../hconverter_8h.html#ad459dce317e77962658ecd25c98b10e2',1,'hconverter.h']]],
+  ['hc_5fhaftarah_5foccasion_4',['hc_haftarah_occasion',['../hc__haftarah_8h.html#afe10dd8a90b2b2d186d76cea1fa3e3c6',1,'hc_haftarah.h']]],
+  ['hc_5fparsha_5',['hc_parsha',['../parshiot_8h.html#a9d1c07ea899fce1bf359f83d526a130c',1,'parshiot.h']]],
+  ['hc_5freading_5fslot_6',['hc_reading_slot',['../hc__torah__reading_8h.html#adc847556ffef0d26a6405ef66fae4967',1,'hc_torah_reading.h']]],
+  ['hc_5fseason_7',['hc_season',['../tekufot_8h.html#a1f1d5a4e9d7f8d54f297f0d8d4ddd057',1,'tekufot.h']]],
+  ['hc_5fspecial_5fday_8',['hc_special_day',['../hc__jewish__dates_8h.html#a37d72f3c286a8f63fe58b1ecc8ab9168',1,'hc_jewish_dates.h']]],
+  ['hc_5ftekufa_5fopinion_9',['hc_tekufa_opinion',['../tekufot_8h.html#ab0524668f0bb43054d90a43222eb146f',1,'tekufot.h']]],
+  ['hc_5fzmanim_10',['hc_zmanim',['../zmanim_8h.html#a1568769eb6a59bd7fb3182cb9764e4f9',1,'zmanim.h']]],
+  ['heb_5ftime_11',['heb_time',['../hconverter_8h.html#ac7a4cd5d0d3b38beae2c34b7ce198938',1,'hconverter.h']]],
+  ['heb_5fyear_5ftype_12',['heb_year_type',['../hconverter_8h.html#add5736c8efbe5e180304777603254226',1,'hconverter.h']]]
 ];
